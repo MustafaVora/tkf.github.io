943adf6155bebff842ec25d46aa0ff4b9f933933
MustafaVora <92010361+MustafaVora@users.noreply.github.com>
2025-10-21 12:27:19 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,10 @@
 const products = [
   { name: "Classic Wood", image: "https://photoframing.in/wp-content/uploads/2025/03/family-photo-frame-8x5-inches-classic-wooden-photo-frame.jpg?auto=format&fit=crop&w=600&q=60" },
   { name: "Golden Glow", image: "https://images.unsplash.com/photo-1526045612212-70caf35c14df?auto=format&fit=crop&w=600&q=60" },
-  { name: "Rustic Heart", image: "https://images.unsplash.com/photo-1499084732479-de2c02d45fc4?auto=format&fit=crop&w=600&q=60" },
   { name: "Black Marble", image: "https://images.unsplash.com/photo-1481277542470-605612bd2d61?auto=format&fit=crop&w=600&q=60" },
   { name: "White Classic", image: "https://images.unsplash.com/photo-1504196606672-aef5c9cefc92?auto=format&fit=crop&w=600&q=60" },
   { name: "Silver Edge", image: "https://images.unsplash.com/photo-1519682337058-a94d519337bc?auto=format&fit=crop&w=600&q=60" },
   { name: "Modern Minimal", image: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?auto=format&fit=crop&w=600&q=60" },
-  { name: "Rosewood Charm", image: "https://images.unsplash.com/photo-1567016555066-5c9184bd5fdf?auto=format&fit=crop&w=600&q=60" },
   { name: "Vintage Vibe", image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=600&q=60" },
   { name: "Nature Frame", image: "https://images.unsplash.com/photo-1518709268805-4e9042af9f23?auto=format&fit=crop&w=600&q=60" },
   { name: "Elegant Border", image: "https://images.unsplash.com/photo-1473187983305-f615310e7daa?auto=format&fit=crop&w=600&q=60" },
